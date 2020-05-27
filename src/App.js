afa474bfab89a3ffe225ff47e75a886ecb2b0ee9
Joe Alongi <datavinivici@outlook.com>
2020-05-27 02:53:26 -0400

    Movie Search Component
    HTML form Search Query with Submit button for searching API.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import SearchMovies from "./searchMovies";
+
 class App extends React.Component {
   render() { 
     return (
       <div className="container">
         <h1 className="title">Movie Night</h1>
+        <SearchMovies/>
       </div>
     );
   }
