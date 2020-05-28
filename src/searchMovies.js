c89a4140a2c6474f4e9ee5dd50f89e9b1354c272
Joe Alongi <datavinivici@outlook.com>
2020-05-28 02:09:35 -0400

    React Hooks for State Management
    Created React Hooks for state management, added 'dotenv' package for processing env, activate search event for query.

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+
+require('dotenv').config()
 
 export default function SearchMovie (){
 
+    const [query, setQuery] = useState('');
+
+    const [movies, setMovies] = useState([]);
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
 
-        const query = "Frozen";
-
-        const url = `https://api.themoviedb.org/3/search/movie?
-        api_key=${process.env.API_KEY}&language=en-US&query=${query}&page=1&
-        include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_NOT_SECRET_CODE}&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data);
+            console.log(data.results);
         }catch(err){
             console.log(err);
         }
@@ -25,7 +26,9 @@ export default function SearchMovie (){
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
-                placeholder="i.e. Frozen"></input>
+                placeholder="i.e. Frozen"
+                value={query} onChange={(e) => setQuery(e.target.value)}
+                />
             <button className="button" type="submit">Search</button>
         </form>
     )
