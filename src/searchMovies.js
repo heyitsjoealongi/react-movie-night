5f909b6b1ce681ac3182c383ca7754454fe9e398
Joe Alongi <datavinivici@outlook.com>
2020-05-28 01:35:08 -0400

    Movie API
    Integrated Movie Database API with Fetch, Promise & Try for error catching.

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 export default function SearchMovie (){
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+
+        const query = "Frozen";
+
+        const url = `https://api.themoviedb.org/3/search/movie?
+        api_key=${process.env.API_KEY}&language=en-US&query=${query}&page=1&
+        include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     return(
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Frozen"></input>
